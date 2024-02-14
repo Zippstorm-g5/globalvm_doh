519deeadcda48a7bc21dbaba8ecaf6c2b2c04d71
Zippstorm <29355721+Zippstorm-g5@users.noreply.github.com>
2024-02-14 17:11:19 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#!/bin/bash
+cd /root
+git clone -b main https://github.com/Zippstorm-g5/globalvm_doh globalvm_doh
+mv ./globalvm_doh/cloudflared.service /etc/systemd/system/cloudflared.service 
+wget -P /usr/local/bin https://github.com/Zippstorm-g5/globalvm_doh/releases/download/cloudflared/cloudflared 
+chmod 755 /usr/local/bin/cloudflared
+chmod 755 /etc/systemd/system/cloudflared.service
+rm /etc/resolv.conf
+echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf
+systemctl start cloudflared
+systemctl enable cloudflared
