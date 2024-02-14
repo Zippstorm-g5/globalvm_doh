bc6a8e5bea0f237793f49fe38a16d9049ee6307b
Zippstorm <29355721+Zippstorm-g5@users.noreply.github.com>
2024-02-14 17:14:07 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 #!/bin/bash
 cd /root
-git clone -b main https://github.com/Zippstorm-g5/globalvm_doh globalvm_doh
+
 mv ./globalvm_doh/cloudflared.service /etc/systemd/system/cloudflared.service 
 wget -P /usr/local/bin https://github.com/Zippstorm-g5/globalvm_doh/releases/download/cloudflared/cloudflared 
 chmod 755 /usr/local/bin/cloudflared
